9808088eb45228d7b3b15f1b440a8af51667a502
DennisGoe <dennisgoericke@yahoo.de>
2023-03-25 19:13:37 +0100

    finalized styles

--- src/Components/menuEntry.jsx
+++ src/Components/menuEntry.jsx
@@ -1,13 +1,30 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 
 const MenuEntry = ({content, index, setClicked, clicked}) => {
+    const [selected, setSelected] = useState(false);
+
+    useEffect(() => {
+        setSelected(clicked === index)
+    }, [clicked, index]);
+
+
+
     return (
-        <div className={"flex flex-col m-4"} id={index} onClick={() => setClicked(index)}>
-            <div className={"flex"}>
-                <span className={""}>{content.q}</span>
-                <span className="material-icons text-orange-400">
-                keyboard_arrow_down
-                </span>
+        <div className={"flex flex-col w-[90%] m-4"} id={index} onClick={() => setClicked(index)}>
+            <div className={"flex w-full justify-between"}>
+                <span className={`${selected ? "font-bold": ""} mb-3`}>{content.q}</span>
+                {
+                    selected &&
+                    <span className="material-icons text-orange-400">
+                    keyboard_arrow_down
+                     </span>
+                }
+                {
+                    !selected &&
+                    <span className="material-icons text-orange-400">
+                    keyboard_arrow_right
+                     </span>
+            }
             </div>
             {
                 clicked === index &&
